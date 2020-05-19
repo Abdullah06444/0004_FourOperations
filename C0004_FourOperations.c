054584d8d9bcd3f3014efba0fc0f587cab219f6b
Abdullah06444 <abdullah44gur@gmail.com>
2020-05-20 01:18:27 +0300

    Update C code snippet

--- C0004_FourOperations.c
+++ C0004_FourOperations.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
 #define SIZE 20
@@ -8,17 +9,17 @@ int main()
 	char user[SIZE];
 
 	printf("Enter username : ");
-	scanf_s("%s", user);
+	scanf("%s", user);
 
 	printf("Hello Hello! Mr/Ms %s , welcome to the c service. :D\n", user);
 
 	float number1, number2;
 
 	printf("Enter 1st number : ");
-	scanf_s("%f", &number1);
+	scanf("%f", &number1);
 
 	printf("Enter 2nd number : ");
-	scanf_s("%f", &number2);
+	scanf("%f", &number2);
 
 	printf("Addition       : %.2f\n", number1 + number2);
 	printf("Subtraction    : %.2f\n", abs(number1 - number2)); // used absolute to the minValue-maxValue for is positive
@@ -28,7 +29,7 @@ int main()
 
 	int number3, number4;
 
-	scanf_s("%d%d", &number3, &number4);
+	scanf("%d%d", &number3, &number4);
 	printf("Mod/Remainder  : %d or : %d\n", number3 % number4, number4 % number3);
 	{
 		int a = number3, b = number4;
